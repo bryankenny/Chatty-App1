f9c4f334c43285d776ede5f53755a85257e8cd3b
Bryan Kenny <b.93.kenny@gmail.com>
2018-11-28 19:54:23 +0000

    passed currentUser to chatbar component

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,33 @@ import MessageList from './MessageList.jsx'
 
 
 class App extends Component {
-  
+  constructor(props) {
+    super(props);
+    // this is the *only* time you should assign directly to state:
+    this.state = {
+      currentUser: {name: "Bryan"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Bryan",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    }
+  }
+
+  // Called after the component was rendered and it was attached to the
+  // DOM. This is a good place to make AJAX requests or setTimeout.
+  componentDidMount() {
+  }
+
   render() {
     return (
       <div>
-       <Navbar /> <ChatBar /> <MessageList />
+       <Navbar /> <ChatBar user={this.state.currentUser}/> <MessageList messages={this.state.messages}/>
       </div>
     );
   }
--- src/Chatbar.jsx
+++ src/Chatbar.jsx
@@ -1,10 +1,19 @@
 import React, {Component} from 'react'
  class ChatBar extends Component {
   render() {
+  
+    var name;
+    if (!this.props.user.name) {
+        return "Anonymous User"
+    }
+    else {
+        name = this.props.user.name
+    }
+
     console.log("Rendering ChatBar")
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-username" placeholder={this.props.user.name} />
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </footer>
     );
